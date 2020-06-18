f7b11bda55ce9d1fd3e56bcf5b0faac40ad415d9
Alexander <erickalexander12@gmail.com>
2020-06-18 11:43:03 +0200

    Updates

---
+++ precache-manifest.18c9f4eea62d38fc1ecb58f13d36e99c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6ba31a8a9bfc134245d",
+    "url": "/organicer/static/css/main.1ade65c7.chunk.css"
+  },
+  {
+    "revision": "e6ba31a8a9bfc134245d",
+    "url": "/organicer/static/js/main.e6ba31a8.chunk.js"
+  },
+  {
+    "revision": "5b3cca693d31b498d11c",
+    "url": "/organicer/static/js/runtime~main.5b3cca69.js"
+  },
+  {
+    "revision": "5d15d4fd344ba7aa1c76",
+    "url": "/organicer/static/js/2.5d15d4fd.chunk.js"
+  },
+  {
+    "revision": "57febd7d34fa91ae2feafaa717f9e933",
+    "url": "/organicer/static/media/marca_organicer_nombre1.57febd7d.png"
+  },
+  {
+    "revision": "7c179fd2255cda3160cf18440791eadf",
+    "url": "/organicer/index.html"
+  }
+];
